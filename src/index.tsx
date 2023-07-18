fccc2cdc27498c12a56bd7a6d277d5155e1fc6c4
MartianAcademy <martianacademy@gmail.com>
2023-07-18 16:00:18 +0530

    service worker unregistered

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,12 @@
-import { ColorModeScript } from '@chakra-ui/react';
+import { ColorModeScript, Spinner } from '@chakra-ui/react';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { RoutesConfig } from './navigation/Routes';
 import ProviderChakra from './providers/ProviderChakra';
-import { ProviderDapp } from './providers/ProviderDApp';
 import { ProviderWeb3Modal } from './providers/ProviderWeb3Modal';
 import * as serviceWorker from './serviceWorker';
-import ("./Pollyfills")
+import('./Pollyfills');
 
 const container = document.getElementById('root');
 if (!container) throw new Error('Failed to find the root element');
@@ -17,11 +16,11 @@ root.render(
   <React.StrictMode>
     <ColorModeScript />
     <ProviderChakra>
-      <ProviderDapp>
+      <React.Suspense fallback={<Spinner />}>
         <ProviderWeb3Modal>
           <RouterProvider router={RoutesConfig}></RouterProvider>
         </ProviderWeb3Modal>
-      </ProviderDapp>
+      </React.Suspense>
     </ProviderChakra>
   </React.StrictMode>
 );
@@ -29,4 +28,4 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorker.register();
+serviceWorker.unregister();
