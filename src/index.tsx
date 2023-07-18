d6450ec56890b2cf042c5a6d9109648e3bbc76e0
MartianAcademy <martianacademy@gmail.com>
2023-07-18 17:15:21 +0530

    wagmi old browser error fixed

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import { ColorModeScript } from '@chakra-ui/react';
+import { ColorModeScript, Spinner } from '@chakra-ui/react';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
@@ -13,14 +13,16 @@ if (!container) throw new Error('Failed to find the root element');
 const root = ReactDOM.createRoot(container);
 
 root.render(
-  <React.Suspense>
+  <React.StrictMode>
     <ColorModeScript />
     <ProviderChakra>
-      <ProviderWeb3Modal>
-        <RouterProvider router={RoutesConfig}></RouterProvider>
-      </ProviderWeb3Modal>
+      <React.Suspense fallback={<Spinner />}>
+        <ProviderWeb3Modal>
+          <RouterProvider router={RoutesConfig}></RouterProvider>
+        </ProviderWeb3Modal>
+      </React.Suspense>
     </ProviderChakra>
-  </React.Suspense>
+  </React.StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
