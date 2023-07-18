3c8b59f4d5f603585c48ef6fab8baef8dafd67a8
MartianAcademy <martianacademy@gmail.com>
2023-07-18 15:03:40 +0530

    service worker activated

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,12 @@
 import { ColorModeScript } from '@chakra-ui/react';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
-import { App } from './App';
-import * as serviceWorker from './serviceWorker';
+import { RouterProvider } from 'react-router-dom';
+import { RoutesConfig } from './navigation/Routes';
 import ProviderChakra from './providers/ProviderChakra';
 import { ProviderDapp } from './providers/ProviderDApp';
 import { ProviderWeb3Modal } from './providers/ProviderWeb3Modal';
-import { RouterProvider } from 'react-router-dom';
-import { BsRouter } from 'react-icons/bs';
-import { RoutesConfig } from './navigation/Routes';
+import * as serviceWorker from './serviceWorker';
 
 const container = document.getElementById('root');
 if (!container) throw new Error('Failed to find the root element');
@@ -30,4 +28,4 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorker.unregister();
+serviceWorker.register();
