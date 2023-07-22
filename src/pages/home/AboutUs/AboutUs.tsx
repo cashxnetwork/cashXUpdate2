7130bd5b3e5c4fc75f970a0f7b004e9ff7433441
MartianAcademy <martianacademy@gmail.com>
2023-07-23 00:04:42 +0530

    why to join divided in 3 paragraphas

--- src/pages/home/AboutUs/AboutUs.tsx
+++ src/pages/home/AboutUs/AboutUs.tsx
@@ -20,12 +20,32 @@ export const AboutUs = () => {
         the token that’s not just a crypto token, but a life changing crypto
         networking concept built on the latest blockchain technology. This is
         the platform where everyone will receive huge referral bonus directly
-        into their wallets by sharing and spreading Cashx Network. CASHX team
-        has no control over how much you can earn. All the transactions are
+        into their wallets by sharing and spreading Cashx Network.
+      </Heading>
+      <Heading
+        size={['md', 'lg']}
+        maxW="5xl"
+        textAlign="center"
+        fontWeight={300}
+      >
+        <Text as="span" fontWeight={900}>
+          CashX
+        </Text>{' '}
+        team has no control over how much you can earn. All the transactions are
         instant and automatic. Early entry into the system is very important as
-        more senior level you reach, higher the upgrade income you receive. Some
-        portion of joining fee goes to $CASHX liquidity which increases the
-        value to the token gradually.
+        more senior level you reach, higher the upgrade income you receive.
+      </Heading>
+      <Heading
+        size={['md', 'lg']}
+        maxW="5xl"
+        textAlign="center"
+        fontWeight={300}
+      >
+        Some portion of joining fee goes to{' '}
+        <Text as="span" fontWeight={900}>
+          CashX
+        </Text>{' '}
+        liquidity which increases the value to the token gradually.
       </Heading>
     </VStack>
   );
