ef9c194dc27d5fee41ec0dac0ca20ec6759b7811
MartianAcademy <martianacademy@gmail.com>
2023-07-24 23:59:22 +0530

    network joining chart border color to white

--- src/pages/home/NetworkJoining/NetworkJoining.tsx
+++ src/pages/home/NetworkJoining/NetworkJoining.tsx
@@ -15,7 +15,7 @@ import { PageWrapper } from '../../../util/PageWrapper';
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 export const NetworkJoining = () => {
-  const chartBorderColor = useColorModeValue('gray.50', 'black');
+  const chartBorderColor = useColorModeValue('white', 'black');
   const data = {
     labels: [
       '$15 | Referral Rewards',
